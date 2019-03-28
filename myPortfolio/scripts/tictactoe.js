37c69cd7d543ca90005e34e36ec8fd88897f3aa5
Zanros421 <michaelhoffmann744@gmail.com>
2019-03-27 19:16:36 -0500

    Got a ticTacToe board.

---
+++ myPortfolio/scripts/tictactoe.js
@@ -0,0 +1,14 @@
+var gameMarker = "X";
+//gameMarker = "O";
+function changeMarkerToX(){
+    gameMarker = "X";
+    console.log("The X button was clicked!");
+}
+function changeMarkerToO(){
+    gameMarker = "O";
+    console.log("The O button was clicked!");
+}
+function placeMark(divId){
+    var box = document.getElementById(divId);
+    box.innerHTML = gameMarker;
+}
