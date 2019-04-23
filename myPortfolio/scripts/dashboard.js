7e4435d294f71c3724a049b887beb1f686dbc9ed
Zanros421 <michaelhoffmann744@gmail.com>
2019-04-22 21:31:51 -0500

    Still not working

--- myPortfolio/scripts/dashboard.js
+++ myPortfolio/scripts/dashboard.js
@@ -1,21 +1,3 @@
-$(function() {
-
-    let $list = $('ul');
-    let $newItemForm = $('#newItemForm');
-  
-    $newItemForm.on('submit', function(e) {
-      e.preventDefault();
-      let text = $('input[type="text"]').val();
-      $list.append(`<li>${text}</li>`);
-      $('input[type="text"]').val('');
-    });
-  
-    $list.on('click', 'li', function() {
-      let $this = $(this);
-      $this.remove();
-    });
-  
-  });
 var tunnels = 0;
 var minerss = 0;
 
@@ -40,6 +22,61 @@ window.setInterval(function(){
   tunnelsClick(miners);
 }, 1000);
 
+// Create a "close" button and append it to each list item
+var myNodelist = document.getElementsByTagName("LI");
+var i;
+for (i = 0; i < myNodelist.length; i++) {
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  myNodelist[i].appendChild(span);
+}
+
+// Click on a close button to hide the current list item
+var close = document.getElementsByClassName("close");
+var i;
+for (i = 0; i < close.length; i++) {
+  close[i].onclick = function() {
+    var div = this.parentElement;
+    div.style.display = "none";
+  }
+}
+
+// Add a "checked" symbol when clicking on a list item
+var list = document.querySelector('ul');
+list.addEventListener('click', function(ev) {
+  if (ev.target.tagName === 'LI') {
+    ev.target.classList.toggle('checked');
+  }
+}, false);
+
+// Create a new list item when clicking on the "Add" button
+function newElement() {
+  var li = document.createElement("li");
+  var inputValue = document.getElementById("myInput").value;
+  var t = document.createTextNode(inputValue);
+  li.appendChild(t);
+  if (inputValue === '') {
+    alert("You must write something!");
+  } else {
+    document.getElementById("myUL").appendChild(li);
+  }
+  document.getElementById("myInput").value = "";
+
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  li.appendChild(span);
+
+  for (i = 0; i < close.length; i++) {
+    close[i].onclick = function() {
+      var div = this.parentElement;
+      div.style.display = "none";
+    }
+  }
+}
 
   //This is real life?
 // Refactored with jQuery!
